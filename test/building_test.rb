4f6bcf4ebb078d6d74b0505e76651abd293d4e31
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-31 11:00:31 -0600

    Add test for renter with highest rent

--- test/building_test.rb
+++ test/building_test.rb
@@ -95,34 +95,23 @@ class BuildingTest < Minitest::Test
     assert_equal renter2,  building.renter_with_highest_rent
 
     renter3 = Renter.new("Max")
+    unit3.add_renter(renter3)
+
+    assert_equal renter2,  building.renter_with_highest_rent
+  end
+
+  def test_it_can_group_by_number_of_bedrooms
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    building.add_unit(unit4)
+
+    assert_equal ({ 3 => ["D4" ], 2 => ["B2", "C3"], 1 => ["A1"]}), building.units_by_number_of_bedrooms
   end
 end
-
-# pry(main)> building.renter_with_highest_rent
-# # => #<Renter:0x00007fa83bc37978...>
-#
-# pry(main)> renter2 = Renter.new("Jessie")
-# # => #<Renter:0x00007fa83b9b0358...>
-#
-# pry(main)> unit1.add_renter(renter2)
-#
-# pry(main)> building.renter_with_highest_rent
-# # => #<Renter:0x00007fa83b9b0358...>
-#
-# pry(main)> renter3 = Renter.new("Max")
-# # => #<Renter:0x00007fa83b7t0456...>
-#
-# pry(main)> unit3.add_renter(renter3)
-#
-# pry(main)> building.renter_with_highest_rent
-# # => #<Renter:0x00007fa83b9b0358...>
-#
-# pry(main)> building.add_unit(unit4)
-#
-# pry(main)> building.units_by_number_of_bedrooms
-# # =>
-# # {
-# #   3 => ["D4" ],
-# #   2 => ["B2", "C3"],
-# #   1 => ["A1"]
-# # }
