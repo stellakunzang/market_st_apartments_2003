a3003275a60e0be2b49a0fd0d47686d73c941267
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-31 10:24:31 -0600

    Add test for rented_units method

--- test/building_test.rb
+++ test/building_test.rb
@@ -56,4 +56,73 @@ class BuildingTest < Minitest::Test
 
     assert_equal 1099.5, building.average_rent
   end
+
+  def test_it_can_find_rented_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+
+    unit2.add_renter(renter1)
+
+    assert_equal [unit2], building.rented_units
+  end
+
+  def test_it_can_find_renter_with_highest_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+
+    assert_equal renter1, building.renter_with_highest_rent
+
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+
+    assert_equal renter2,  building.renter_with_highest_rent
+
+    renter3 = Renter.new("Max")
+  end
 end
+
+# pry(main)> building.renter_with_highest_rent
+# # => #<Renter:0x00007fa83bc37978...>
+#
+# pry(main)> renter2 = Renter.new("Jessie")
+# # => #<Renter:0x00007fa83b9b0358...>
+#
+# pry(main)> unit1.add_renter(renter2)
+#
+# pry(main)> building.renter_with_highest_rent
+# # => #<Renter:0x00007fa83b9b0358...>
+#
+# pry(main)> renter3 = Renter.new("Max")
+# # => #<Renter:0x00007fa83b7t0456...>
+#
+# pry(main)> unit3.add_renter(renter3)
+#
+# pry(main)> building.renter_with_highest_rent
+# # => #<Renter:0x00007fa83b9b0358...>
+#
+# pry(main)> building.add_unit(unit4)
+#
+# pry(main)> building.units_by_number_of_bedrooms
+# # =>
+# # {
+# #   3 => ["D4" ],
+# #   2 => ["B2", "C3"],
+# #   1 => ["A1"]
+# # }
