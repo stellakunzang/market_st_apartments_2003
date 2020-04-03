12ba0f6c8b8ffd369970603184245c83a3ffcca1
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-03 12:56:49 -0600

    Refactor based on instructor walkthrough

--- lib/building.rb
+++ lib/building.rb
@@ -10,53 +10,65 @@ class Building
   end
 
   def renters
-    renters = []
-    @units.each do |unit|
-      if unit.renter != nil
-        renters << unit.renter.name
-      end
+    rented_units.map do |unit|
+      unit.renter.name
     end
-    renters
+    # renters = []
+    # @units.each do |unit|
+    #   if unit.renter != nil
+    #     renters << unit.renter.name
+    #   end
+    # end
+    # renters
   end
 
   def average_rent
-    rents = 0.0
-    count = 0
-    @units.each do |unit|
-      rents += unit.monthly_rent
-      count += 1
-    end
-    rents/count
+    total = (@units.sum {|unit| unit.monthly_rent}).to_f
+    (total / @units.length.to_f).round(2)
+    # rents = 0.0
+    # count = 0.0
+    # @units.each do |unit|
+    #   rents += unit.monthly_rent
+    #   count += 1
+    # end
+    # rents/count
   end
 
   def rented_units
-    rented_units = []
-    @units.each do |unit|
-      if unit.renter != nil
-        rented_units << unit
-      end
+    @units.find_all do |unit|
+      unit.renter
     end
-    rented_units
+    # rented_units = []
+    # @units.each do |unit|
+    #   if unit.renter != nil
+    #     rented_units << unit
+    #   end
+    # end
+    # rented_units
   end
 
-  def sort_by_rent
-    @units.sort_by do |unit|
-      unit.monthly_rent
-    end
-  end
-
-  def units_with_renters
-    remove_nil_renters = []
-    sort_by_rent.each do |unit|
-      if unit.renter != nil
-        remove_nil_renters << unit
-      end
-    end
-    remove_nil_renters
-  end
+  # def sort_by_rent
+  #   @units.sort_by do |unit|
+  #     unit.monthly_rent
+  #   end
+  # end
+  #
+  # def units_with_renters
+  #   remove_nil_renters = []
+  #   sort_by_rent.each do |unit|
+  #     if unit.renter != nil
+  #       remove_nil_renters << unit
+  #     end
+  #   end
+  #   remove_nil_renters
+  # end
 
   def renter_with_highest_rent
-    units_with_renters[-1].renter
+    # units_with_renters[-1].renter
+    highest_rent = rented_units.max_by do |unit|
+      unit.monthly_rent
+    end
+    highest_rent.renter
   end
 
   # def units_by_number_of_bedrooms
@@ -66,20 +78,25 @@ class Building
   # end
   #
   def units_by_number_of_bedrooms
-    units_by_num_bed = {}
+    units_by_num_bed = Hash.new { |hash, key| hash[key] =  [] }
     @units.each do |unit|
-      if units_by_num_bed[unit.bedrooms] == nil
-        units_by_num_bed[unit.bedrooms] = [unit.number]
-      else
-        units_by_num_bed[unit.bedrooms] << unit.number
-      end
+      units_by_num_bed[unit.bedrooms] << unit.number
     end
     units_by_num_bed
+    # units_by_num_bed = {}
+    # @units.each do |unit|
+    #   if units_by_num_bed[unit.bedrooms] == nil
+    #     units_by_num_bed[unit.bedrooms] = [unit.number]
+    #   else
+    #     units_by_num_bed[unit.bedrooms] << unit.number
+    #   end
+    # end
+    # units_by_num_bed
   end
 
   def annual_breakdown
     breakdown = {}
-    units_with_renters.each do |unit|
+    rented_units.each do |unit|
       breakdown[unit.renter.name] = (unit.monthly_rent * 12)
     end
     breakdown
@@ -87,7 +104,7 @@ class Building
 
   def rooms_by_renter
     rooms = {}
-    units_with_renters.reverse.each do |unit|
+    rented_units.reverse.each do |unit|
       rooms[unit.renter] = {bathrooms: unit.bathrooms, bedrooms: unit.bedrooms}
     end
     rooms
